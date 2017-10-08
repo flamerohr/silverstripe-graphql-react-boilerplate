c3a620f444b20a214811a27dc23f3672944efbeb
Christopher Joe <flamer.ohr@gmail.com>
2017-10-09 09:26:34 +1300

    Remove redundant readResolver method

--- src/Controllers/TodoApp.php
+++ src/Controllers/TodoApp.php
@@ -1,15 +1,10 @@
 <?php
 namespace Flamerohr\Todo\Controllers;
 
-use Exception;
-use Flamerohr\Todo\Models\Todo;
-use GraphQL\Type\Definition\ResolveInfo;
 use SilverStripe\Control\Controller;
 use SilverStripe\Control\Director;
 use SilverStripe\Control\HTTPRequest;
-use SilverStripe\ORM\DataList;
 use SilverStripe\ORM\DataModel;
-use SilverStripe\ORM\DataObjectInterface;
 use SilverStripe\Security\PermissionProvider;
 
 class TodoApp extends Controller implements PermissionProvider
@@ -34,27 +29,6 @@ class TodoApp extends Controller implements PermissionProvider
     public function handleRequest(HTTPRequest $request, DataModel $model = null)
     {
         return $this->render()->forTemplate();
-    }
-    
-    /**
-     * @param DataObjectInterface$obj
-     * @param $args
-     * @param $context
-     * @param ResolveInfo $info
-     * @return DataList $list
-     * @throws Exception
-     */
-    public function readResolver(DataObjectInterface $obj, $args, $context, ResolveInfo $info)
-    {
-        if (!singleton(Todo::class)->canView($context['currentMember'])) {
-            throw new Exception('Cannot view Todo');
-        }
-        $list = Todo::get();
-        if (isset($args['ID'])) {
-            $list = $list->filter('ID', $args['ID']);
-        }
-
-        return $list;
     }
 
     public function providePermissions()
